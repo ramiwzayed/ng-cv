3fc5994403c426925e363e3074801865d9592043
Extreme <ramiwzayed@gmail.com>
2024-03-07 01:07:49 +0200

    Create Skills component

---
+++ src/app/components/skill/skill.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-skill',
+  standalone: true,
+  imports: [],
+  templateUrl: './skill.component.html',
+  styleUrl: './skill.component.css'
+})
+export class SkillComponent {
+  @Input({required: true}) skill!: string;
+
+}
