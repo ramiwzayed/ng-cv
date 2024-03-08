eefd946a37edde2d3fe7d94fd5f590a6b127889a
Extreme <ramiwzayed@gmail.com>
2024-03-08 02:20:38 +0200

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NgOptimizedImage} from "@angular/common";
 import {SkillComponent} from "./components/skill/skill.component";
+import {WorksExperienceComponent} from "./components/works-experience/works-experience.component";
+import {InformationComponent} from "./components/information/information.component";
+import {WorkType} from "./types/work.type";
+import {infoType} from "./types/information.type";
 
-// import {WorkComponent} from "./components/work/work.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, SkillComponent],
+  imports: [RouterOutlet, NgOptimizedImage, SkillComponent, WorksExperienceComponent, InformationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,12 +24,50 @@ export class AppComponent {
     "Adobe Indesign",
     "HTML-5/CSS-3"
   ]
+
+  allWorks: WorkType[] = [
+    {
+      date: "2012-2014",
+      title: "tile",
+      companyName: "Company Name",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, placeat."
+    },
+    {
+      date: "2014-2017",
+      title: "tile",
+      companyName: "Company Name",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, placeat."
+    },
+    {
+      date: "2017-Present",
+      title: "tile",
+      companyName: "Company Name",
+      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, placeat."
+    }
+  ]
+
+  allInfo: infoType[] = [
+    {
+      major: "Name of your major",
+      university: "Name Of your university",
+      date: "2005 - 2009",
+
+    },
+    {
+      major: "Name of your major",
+      university: "Name Of your university",
+      date: "2009 - 2011",
+
+    }
+  ]
+  aboutMeText: string = "Lorem Ipsum dolor sit amet , consectetur adipiscing elit,sed do euusmod empor incididunt ut labore et dolore magna alique";
 }
 
-// export class ComponentApp {
-//   work: string[] = [
-//     "2012-2014 Job Position Here",
-//     "2014-2017 Job Position Here",
-//     "2017-Present Job Position Here"
-//   ]
-// }
+// phone: string,
+//   email: string,
+//   website: string,
+//   address: string
+// phone: "enter your phone number",
+//   email: "email",
+//   website: "website",
+//   address: "address"
